148e8836e4849fb8ca8c9994a4ffd9053a5bb327
awkward crow <awkward.crow@gmail.com>
2024-03-15 15:55:54 +0000

    stop_words function

--- src/nml.ml
+++ src/nml.ml
@@ -3,9 +3,13 @@
 (* pride-and-prejudice.txt stop_words.txt *)
 
 open Core
+open Printf
 
-let () = 
-    In_channel.read_all "./stop_words.txt" |> print_endline
+let stop_words filename =
+    String.split ~on:',' (In_channel.read_all filename)
+
+let _ = 
+    List.iter ~f:(printf "%s ") (stop_words "./stop_words.txt")
     
 
 
