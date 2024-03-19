996d83f834289e2c8ad709a776f858fdc038dbe4
byte-glass <m.campbell@tutanota.com>
2024-03-19 10:24:43 +0000

    revisit nml

--- src/nml.ml
+++ src/nml.ml
@@ -1,6 +1,13 @@
 (* nml.ml *)
 
-(* pride-and-prejudice.txt stop_words.txt *)
+(* 
+   usage: in top level directory
+
+    #use "topfind";;
+    #require "pcre";;
+
+    #use "src/nml.ml";;
+*)
 
 open Core
 open Printf
@@ -8,8 +15,19 @@ open Printf
 let stop_words filename =
     String.split ~on:',' (In_channel.read_all filename)
 
-let _ = 
-    List.iter ~f:(printf "%s ") (stop_words "./stop_words.txt")
+let z = List.map ~f:char_of_int (List.range (97) (97 + 26))
+
+let words filename =
+    let w = In_channel.read_all filename and
+        r = Pcre.regexp "[\\W_]+" in
+    String.lowercase (Pcre.replace ~rex:r ~templ:" " w) |> String.split ~on:' '
+
+let _ =
+    List.iter ~f:(printf "%s ") (stop_words "./stop_words.txt");
+    print_endline "";
+    List.iter ~f:(printf "%s ") (words "./pride-and-prejudice.txt");
+    print_endline ""
+
     
 
 
